264b79b2f3f32626988c1c50eba63b788a17efcf
Sacha.H <sacha.hamiche@gmail.com>
2019-02-12 22:59:57 +0100

    Add Recursive_Size_Calcul and update Delete/Create/Update

--- swagger_server/models/file_model.py
+++ swagger_server/models/file_model.py
@@ -16,6 +16,24 @@ import shutil
 
 ## GENERIQUE FUNCTION FOR MODEL FILE
 
+def recursive_update_size(path_final, info_user):
+    connection = get_connexion()
+
+    with connection.cursor() as cursor:
+        if !os.path.exists(path_final):
+            path_final = path_final.rsplit('/',1)[0]
+        else:
+            sql = """UPDATE `ld_accounts`SET `size`=%s WHERE path = %s and id_storage=%s"""
+            cursor.execute(sql,  (os.path.getsize(path_final),path_final.replace(info_user['path_home']+"/",""),info_user['id_storage']))
+
+        while (path_final != info_user['path_home']):
+            sql = """UPDATE `ld_accounts`SET `size`=%s WHERE path = %s and id_storage=%s"""
+            cursor.execute(sql,  (os.path.getsize(path_final),path_final.replace(info_user['path_home']+"/",""),info_user['id_storage']))
+            path_final = path_final.rsplit('/',1)[0]
+        sql = """UPDATE `oc_storages`SET `used_space`=%s WHERE uid = %s"""
+        cursor.execute(sql,  (os.path.getsize(info_user['path_home']+"/"),info_user['path_home'],info_user['user_id']))
+        connection.commit()
+        connection.close()
 def get_user_info(username):
     connection = get_connexion()
 
@@ -106,23 +124,10 @@ def upload_file_model(username, path_file, file, propertyname, propertyvalue):
             sql2 = "INSERT INTO `ld_filecache` (`id_storage`, `path`, `path_hash`, `name`, `mime_type`, `size`, `storage_mtime`, `id_parent`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
             cursor.execute(sql2, (info_user['id_storage'],path_upload.replace(info_user['path_home']+"/",""),hash_pathupload,magic.from_file(path_upload),magic.from_file(path_upload, mime=True),file_size,tstamp_now,id_parent))
 
-            # Get the id of insert in filecache
 
-            sql45 = "SELECT id, mime_type, path as path_file, name as type, storage_mtime, size FROM ld_filecache WHERE id=(SELECT LAST_INSERT_ID())"
-            cursor.execute(sql45)
-
-            info_fileinsert = cursor.fetchone()
-            info_fileinsert['name'] = info_fileinsert['path_file'].split('/')[-1]
-
-            # Update folder of file in BDDD
-
-            sql3 = "UPDATE `ld_filecache` SET `size`=`size`+%s WHERE `id_storage`=%s AND `path`=%s"
-            cursor.execute(sql3, (file_size,info_user['id_storage'],path_final.replace(info_user['path_home']+"/","")))
-
-            sql4 = "UPDATE `oc_storages` SET `used_space`=`used_space`+%s WHERE `id`=%s AND `uid`=%s"
-            cursor.execute(sql4, (file_size,info_user['id_storage'],info_user['user_id']))
         connection.commit()
         connection.close()
+        recursive_update_size(path_file, info_user)
     except:
         traceback.print_exc()
         return json_output(400,"bad request, check information passed through API")
@@ -272,6 +277,7 @@ def delete_file_model(username, id_file):
             cursor.execute(sql2, (info_user["id_storage"],id_file))
             info_file = cursor.fetchone()
             path_file = os.path.join(info_user["path_home"], info_file['path'])
+            size = info_file['size']
 
             if (info_file['name'] == 'Folder'):
                 shutil.rmtree(path_file)
@@ -283,8 +289,11 @@ def delete_file_model(username, id_file):
                 os.remove(path_file)
                 sql5 = "DELETE FROM ld_filecache WHERE id_storage=%s AND id=%s"
                 cursor.execute(sql5, (info_user["id_storage"],id_file))
+
+
             connection.commit()
             connection.close()
+            recursive_update_size(path_file, info_user)
             return json_output(200,"successful operation")
 
     except:
@@ -301,20 +310,32 @@ def delete_file_model(username, id_file):
             connection = get_connexion()
 
             with connection.cursor() as cursor:
+
                 # get info of user
                 info_user = get_user_info(username)
+                sql2 = "SELECT * FROM ld_filecache WHERE id_storage=%s AND id=%s"
+                cursor.execute(sql2, (info_user["id_storage"],id_file))
+                info_file = cursor.fetchone()
 
                 # Create folder if not exists !
                 path_final = recursive_create_dir(path_file, info_user)
+                hash_pathupload = dirhash(path_final, 'md5')
                 tstamp_now = int(time.time())
 
                 # Upload file on the server
                 filename = file.filename
                 path_upload = os.path.join(path_final, filename)
-
                 if !os.path.exists(path_upload):
                     return json_output(409,"File/Folder doesn't exist")
+                #Rename folder
+                if os.path.isdir(info_file['path']):
+                    os.rename(info_file['path'],path_upload)
+                    path_sql = info_file['path'] + '%'
+                    sql4 = "UPDATE `ld_filecache` SET `path` = %s,`path_hash` = %s  WHERE id_storage=%s AND  path LIKE %s"
+                    cursor.execute(sql4, (path_upload.replace(info_user['path_home']+"/",""),hash_pathupload,info_user["id_storage"],info_file['path']))
+                    return json_output(409,"Folder renamed")
 
+                delete_file_model(username, id_file)
                 file.save(path_upload)
 
                 # Check if space enough
@@ -323,18 +344,18 @@ def delete_file_model(username, id_file):
                 if info_user['quota'] < (info_user['used_space']+file_size):
                     os.remove(path_upload)
                     return json_output(409,"not enough space available")
-
                 # Insert info file in BDD
 
                 id_parent = get_dir_parent(path_upload,info_user)
 
-                hash_pathupload = dirhash(path_final, 'md5')
+
                 sql2 = "UPDATE `ld_filecache` SET `path` = %s, `path_hash` = %s, `name` = %s, `mime_type`=%s, `size`=%s, `storage_mtime`=%s WHERE id=%s AND id_storage=%s"
                 cursor.execute(sql2, (path_upload.replace(info_user['path_home']+"/",""),hash_pathupload,magic.from_file(path_upload),magic.from_file(path_upload, mime=True),file_size,tstamp_now,id_file, info_user["id_storage"]))
 
 
             connection.commit()
             connection.close()
+            recursive_update_size(path_upload, info_user)
         except:
             traceback.print_exc()
             return json_output(400,"bad request, check information passed through API")
